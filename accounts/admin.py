646d6dde7f6bd33a57c50c6ad899206a961b943e
lampurights <lampurights1381@gmail.com>
2024-03-01 16:33:06 +0330

    first chapter

---
+++ accounts/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+# Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+    list_display = ['username', 'email']
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
